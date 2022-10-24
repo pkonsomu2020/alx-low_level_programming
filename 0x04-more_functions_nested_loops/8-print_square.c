49a9065c3e6dd89b7abe46922ec3984a760d29db
pkonsomu2020 <pkonsomu2020@gmail.com>
2022-10-24 07:04:40 -0700

    Task eight

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * prints_square - print a square
+ * @size: size of the square
+ */
+
+void print_square(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i = 0, j;
+
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
