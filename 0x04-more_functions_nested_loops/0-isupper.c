db7c7e8a08a82dc2546f1394916ff0d698ce9d20
pkonsomu2020 <pkonsomu2020@gmail.com>
2022-10-24 05:10:03 -0700

    Task zero

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * _isupper - checks uppercase
+ * _isupper - checks if a character is uppercase
  * @c: parameter
  * Return: 0 if successful
  */
 
 int _isupper(int c)
 {
-	if (c >= 65 && c < 91)
+	if (c >= 'A' && c <= 'Z')
 	{
 		return (1);
 	}
