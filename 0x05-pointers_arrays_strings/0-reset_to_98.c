dda00be59146baace9499cad9b22d1cef7c50e08
pkonsomu2020 <pkonsomu2020@gmail.com>
2022-10-25 09:47:12 -0700

    Task zero

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - update the value of n to 98
+ * @n: input
+ * Return: Always 0 (success)
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
